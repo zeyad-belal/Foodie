9432b072c7fd6c1e35d1085d5a33960d9cbea23c
zeyad-belal <zeyadbelal00@gmail.com>
2023-07-10 02:28:54 +0300

    toolkit fix

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -52,60 +52,6 @@ const cartSlice = createSlice({
   }
 })
 
-// const CartReducer = (state = defaultCartState, action) => {
-//   if (action.type === "ADD") {
-//     const updatedTotalAmount =
-//       state.totalAmount + action.item.amount * action.item.price;
-
-//     const existingCartItemIndex = state.items.findIndex(
-//       (item) => item.id === action.item.id
-//     );
-//     const existingCartItem = state.items[existingCartItemIndex];
-//     let updatedItems;
-
-//     if (existingCartItem) {
-//       const updatedItem = {
-//         ...existingCartItem,
-//         amount: existingCartItem.amount + action.item.amount,
-//       };
-
-//       updatedItems = [...state.items];
-//       updatedItems[existingCartItemIndex] = updatedItem;
-//     } else {
-//       updatedItems = [...state.items, action.item];
-//     }
-
-//     return {
-//       items: updatedItems,
-//       totalAmount: updatedTotalAmount,
-//     };
-//   } else if (action.type === "REMOVE") {
-//     const existingCartItemIndex = state.items.findIndex(
-//       (item) => item.id === action.id
-//     );
-
-//     const existingItem = state.items[existingCartItemIndex];
-
-//     const updatedTotalAmount = state.totalAmount - existingItem.price;
-
-//     let updatedItems;
-
-//     if (existingItem.amount === 1) {
-//       updatedItems = state.items.filter((item) => item.id !== action.id);
-//     } else {
-//       const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
-//       updatedItems = [...state.items];
-//       updatedItems[existingCartItemIndex] = updatedItem;
-//     }
-//     return {
-//       items: updatedItems,
-//       totalAmount: updatedTotalAmount,
-//     };
-//   } else if (action.type === "CLEAR") {
-//     return defaultCartState;
-//   }
-//   return state;
-// };
 
 const store = configureStore({
   reducer: cartSlice.reducer
